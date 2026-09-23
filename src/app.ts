33fa90910a1695eb12e1d0760a937339f971eef7
s00265100 <s00265100@atu.ie>
2026-09-23 10:48:51 +0100

    database.ts added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, {Application, Request, Response} from "express";
 import { env } from "./config/env";
 import carRoutes from './routes/cars';
+import { connectDB } from "./config/database";
 
 const PORT = env.port;
 
@@ -30,7 +31,11 @@ app.use((req, _res, next) => {
     next();
 });
 //comment
-app.listen(PORT, () => {
-    console.log("Server is running on port", PORT);
-    });
+const startServer = async () => {
+  await connectDB();
 
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+startServer();
--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const env = {
-
+mongoURI: process.env.MONGODB_URI ?? "",
 port: Number(process.env.PORT ?? 3001),
 nodeEnv: process.env.NODE_ENV ?? "development",
 
